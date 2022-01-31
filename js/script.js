64b68b89193bcb7140aad6b94144d462e0ad5fde
jordan <JordansGit@users.noreply.github.com>
2022-01-31 23:03:33 +0000

    init project, finished html and css

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*
+.share-popup -> swap between display flex, display none; 
+when button is clicked: 
+  the above 
+  change circle bg color to the dark color 
+  change color of circle img to body color
+*/
